ef9f8adfec202a13b6c4689b4a30acb6ef638d7a
yongjin <dididi363@naver.com>
2026-07-22 15:23:39 +0900

    requirements 수정2

--- law-crm-backend/database.py
+++ law-crm-backend/database.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres.anhcvndjvypucrneqhvq:tpscl!13467@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres"
-)
-# PostgreSQL 연결 설정 (SQLite 전용 옵션이었던 check_same_thread는 제거됨)
+# 환경 변수에서만 안전하게 가져오기 (하드코딩 제거)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL 환경 변수가 설정되지 않았습니다!")
+
+# PostgreSQL 연결 설정
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
